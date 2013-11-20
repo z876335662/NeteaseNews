d543c15fb30e5752ee462f42e144b385f6a87166
yvon <yvon@yvon-PC>
2013-11-20 22:40:23 +0800

    添加资源

---
+++ src/com/yvon/client/biz/ad/AdActivity.java
@@ -0,0 +1,13 @@
+package com.yvon.client.biz.ad;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class AdActivity extends FragmentActivity{
+	
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+	}
+
+}
